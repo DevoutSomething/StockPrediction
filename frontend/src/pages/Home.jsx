733e899471827a5ecd04e0262b91048fe257fb41
lukeeskinner <u1440351@umail.utah.edu>
2025-02-28 22:01:28 -0700

    Updated home page CSS

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import Header from "../components/Header"
-import Body from "../components/Body"
-import "../components/Styles/body.css"
+import Header from "../components/Header";
+import Body from "../components/Body";
+import ParamsButton from "../components/ParamsBtn";
+import "../components/Styles/body.css";
 
 export default function Home() {
   return (
@@ -9,6 +10,5 @@ export default function Home() {
       <Body />
       <ParamsButton route="/params" />
     </div>
-  )
+  );
 }
-
