70628116a233c19c0be77f552c0874d7b1cd4c7b
ArcherFox <archer.l.fox@gmail.com>
2025-03-01 13:24:57 -0700

    edited some stuff

--- frontend/src/components/ChartHeader.jsx
+++ frontend/src/components/ChartHeader.jsx
@@ -1,10 +1,9 @@
-import "./styles/chartHeader.css"
+import "./styles/chartHeader.css";
 
 export default function ChartHeader() {
   return (
     <div className="chart-header">
-      <h1>Generated Stocks</h1>
+      <h1 className="chart-text">Generated Stocks</h1>
     </div>
-  )
+  );
 }
-
