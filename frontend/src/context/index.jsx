f346b4fe4ae839fd87fa2bf1b1fe2d297f7717e2
ArcherFox <archer.l.fox@gmail.com>
2025-02-28 22:06:24 -0700

    Added global state and changed payment fields to work with global state

--- frontend/src/context/index.jsx
+++ frontend/src/context/index.jsx
@@ -17,7 +17,6 @@ export default function GlobalState({ children }) {
         setProfit,
       }}
     >
-      {" "}
       {children}
     </GlobalContext.Provider>
   );
--- frontend/src/pages/Params.jsx
+++ frontend/src/pages/Params.jsx
@@ -1,3 +1,50 @@
+import Header from "../components/Header";
+import "../components/Styles/params.css";
+import { GlobalContext } from "../context";
+import { useContext } from "react";
+
 export default function Params() {
-  return <p>params</p>;
+  console.log(GlobalContext);
+  const { payment, setPayment, time, setTime, profit, setProfit } =
+    useContext(GlobalContext);
+  return (
+    <div className="params-page">
+      <Header />
+      <div className="form-container">
+        <div className="input-group">
+          <label htmlFor="payment">Payment</label>
+          <input
+            type="number"
+            id="payment"
+            name="payment"
+            placeholder="Enter Payment"
+            value={payment}
+            onChange={(e) => setPayment(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="time">Time</label>
+          <input
+            type="number"
+            id="time"
+            name="time"
+            placeholder="Enter Time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="profit">Profit</label>
+          <input
+            type="number"
+            id="profit"
+            name="profit"
+            placeholder="Enter Profit"
+            value={profit}
+            onChange={(e) => setProfit(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
