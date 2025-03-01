a58f00288266c7b455263cde1afccfe52f40529d
ArcherFox <archer.l.fox@gmail.com>
2025-03-01 02:47:44 -0700

    fixed the shit page

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -8,30 +8,35 @@ export default function FormContainer() {
   const { payment, setPayment, time, setTime, profit, setProfit } =
     useContext(GlobalContext);
 
+  // Check if all fields are set
+  const allFieldsSet = payment && time && profit;
+
   return (
-    <div className="form-container">
-      <Form
-        isFirst={true}
-        state={payment}
-        setState={setPayment}
-        text="Payment"
-        id="payment"
-        defaultText="Enter Payment"
-      />
-      <Form
-        state={profit}
-        setState={setProfit}
-        text="Profit"
-        id="profit"
-        defaultText="Enter Profit"
-      />
-      <TimeForm
-        state={time}
-        setState={setTime}
-        text="Time"
-        id="time"
-        defaultText="Enter Time"
-      />
+    <div className={`form-container-wrapper ${allFieldsSet ? "filled" : ""}`}>
+      <div className="form-container">
+        <Form
+          isFirst={true}
+          state={payment}
+          setState={setPayment}
+          text="Payment"
+          id="payment"
+          defaultText="Enter Payment"
+        />
+        <Form
+          state={profit}
+          setState={setProfit}
+          text="Profit"
+          id="profit"
+          defaultText="Enter Profit"
+        />
+        <TimeForm
+          state={time}
+          setState={setTime}
+          text="Time"
+          id="time"
+          defaultText="Enter Time"
+        />
+      </div>
     </div>
   );
 }
